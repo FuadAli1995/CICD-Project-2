eeb87e9873a5a1f1d21c9f58dab60eff6b30a6ed
Fuad Ali <fuadali19952017@gmail.com>
2025-12-13 20:01:07 +0000

    adding workflow

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import MySQLdb
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def hello_world():
     # Connect to the MySQL database
@@ -18,6 +20,8 @@ def hello_world():
     cur.execute("SELECT VERSION()")
     version = cur.fetchone()
     return f'Hello, World! MySQL version: {version[0]}'
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
