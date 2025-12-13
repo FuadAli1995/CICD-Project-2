271d5a299998328084ab6d81537ff04ac2582363
Fuad Ali <fuadali19952017@gmail.com>
2025-12-13 20:02:15 +0000

    adding workflow

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
-# app.py
-
 from flask import Flask
 import MySQLdb
 
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def hello_world():
     # Connect to the MySQL database
     db = MySQLdb.connect(
-        host="mydb",    # Hostname of the MySQL container
-        user="root",    # Username to connect to MySQL
-        passwd="my-secret-pw",  # Password for the MySQL user
-        db="mysql"      # Name of the database to connect to
+        host="mydb",
+        user="root",
+        passwd="my-secret-pw",
+        db="mysql"
     )
     cur = db.cursor()
     cur.execute("SELECT VERSION()")
@@ -22,6 +19,6 @@ def hello_world():
     return f'Hello, World! MySQL version: {version[0]}'
 
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
+
